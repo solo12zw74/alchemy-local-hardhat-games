c4831efe7f9bc2088345e20f82fd4250fd8be541
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-05-11 22:14:54 -0600

    Solve all 5 games
    Deploy all 5 contracts at a time and solve them all at a time

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,10 +2,26 @@
 const contractName = "Game1";
 
 async function main() {
-  const Game = await hre.ethers.getContractFactory(contractName);
-  // if you need to add constructor arguments for the particular game, add them here:
-  const game = await Game.deploy();
-  console.log(`${contractName} deployed to address: ${game.address}`);
+  const Game1 = await hre.ethers.getContractFactory("Game1");
+  const game1 = await Game1.deploy();
+  console.log(`Game1 deployed to address: ${game1.address}`);
+
+  const Game2 = await hre.ethers.getContractFactory("Game2");
+  const game2 = await Game2.deploy();
+  console.log(`Game2 deployed to address: ${game2.address}`);
+
+  const Game3 = await hre.ethers.getContractFactory("Game3");
+  const game3 = await Game3.deploy();
+  console.log(`Game3 deployed to address: ${game3.address}`);
+
+  const Game4 = await hre.ethers.getContractFactory("Game4");
+  const game4 = await Game4.deploy();
+  console.log(`Game4 deployed to address: ${game4.address}`);
+
+  const Game5 = await hre.ethers.getContractFactory("Game5");
+  const game5 = await Game5.deploy();
+  console.log(`Game5 deployed to address: ${game5.address}`);
+
 }
 
 main()
--- scripts/win.js
+++ scripts/win.js
@@ -1,18 +1,37 @@
-// add the game address here and update the contract name if necessary
-const gameAddr = "";
-const contractName = "Game1";
-
 async function main() {
-    // attach to the game
-    const game = await hre.ethers.getContractAt(contractName, gameAddr);
+  const game1 = await hre.ethers.getContractAt("Game1", "0xFD471836031dc5108809D173A067e8486B9047A3");
+  const tx1 = await game1.win();
+  const receipt1 = await tx1.wait();
+  console.log("Receipt #1");
+  console.debug(receipt1);
 
-    // do whatever you need to do to win the game here:
-    const tx = await game.win();
+  const game2 = await hre.ethers.getContractAt("Game2", "0xcbEAF3BDe82155F56486Fb5a1072cb8baAf547cc");
+  await game2.setX(25);
+  await game2.setY(25);
+  const tx2 = await game2.win();
+  const receipt2 = await tx2.wait();
+  console.log("Receipt #2");
+  console.debug(receipt2);
 
-    // did you win? Check the transaction receipt!
-    // if you did, it will be in both the logs and events array
-    const receipt = await tx.wait();
-    console.log(receipt);
+  const game3 = await hre.ethers.getContractAt("Game3", "0x1429859428C0aBc9C2C47C8Ee9FBaf82cFA0F20f");
+  const tx3 = await game3.win(45);
+  const receipt3 = await tx3.wait();
+  console.log("Receipt #3");
+  console.debug(receipt3);
+
+  const game4 = await hre.ethers.getContractAt("Game4", "0xB0D4afd8879eD9F52b28595d31B441D079B2Ca07");
+  const tx4 = await game4.win(56);
+  const receipt4 = await tx4.wait();
+  console.log("Receipt #4");
+  console.debug(receipt4);
+
+  const game5 = await hre.ethers.getContractAt("Game5", "0x162A433068F51e18b7d13932F27e66a3f99E6890");
+  await game5. giveMeAllowance(10001);
+  await game5.mint(10000);
+  const tx5 = await game5.win();
+  const receipt5 = await tx5.wait();
+  console.log("Receipt #5");
+  console.debug(receipt5);
 }
 
 main()
